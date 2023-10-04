056876f6a4502b67c10ea889a59c5e8dbfbc0e77
KeddyB <bokelusi@gmail.com>
2023-10-04 10:02:16 +0100

    commit

--- register.php
+++ register.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style/style.css">
+    <link rel="stylesheet" href="style/styler.css">
     <title>Register</title>
 </head>
 <body>
@@ -67,7 +67,7 @@
                     <input type="submit" class="btn" name="submit" value="Register" required>
                 </div>
                 <div class="links">
-                    Already a member? <a href="index.php">Sign In</a>
+                    Already a member? <a href="login.php">Sign In</a>
                 </div>
             </form>
         </div>
