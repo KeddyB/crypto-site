98b51d7cabce5ae5be65f449df6429df70c1e453
KeddyB <bokelusi@gmail.com>
2023-10-05 09:11:00 +0100

    styler.css

--- home.php
+++ home.php
@@ -12,17 +12,16 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style/style.css">
+    <link rel="stylesheet" href="style/styler.css">
     <title>Home</title>
 </head>
 <body>
     <div class="nav">
         <div class="logo">
-            <p><a href="home.php">Logo</a> </p>
+            <p><a href="home.php">Crypto Trading</a></p>
         </div>
 
         <div class="right-links">
-
             <?php 
             
             $id = $_SESSION['id'];
@@ -37,24 +36,24 @@
             echo "<a href='edit.php?Id=$res_id'>Change Profile</a>";
             ?>
 
-            <a href="php/logout.php"> <button class="btn">Log Out</button> </a>
+            <a href="php/logout.php"><button class="btn">Log Out</button> </a>
 
         </div>
     </div>
     <main>
 
-       <div class="main-box top">
+       <div class="main-box">
           <div class="top">
             <div class="box">
                 <p>Hello <b><?php echo $res_Uname ?></b>, Welcome</p>
             </div>
             <div class="box">
-                <p>Your email is <b><?php echo $res_Email ?></b>.</p>
+                <p>Your balance is <b>ETH undefined</b>.</p>
             </div>
           </div>
           <div class="bottom">
             <div class="box">
-                <p>And you are fine </p> 
+                <p>Payment Wallet </p> 
             </div>
           </div>
        </div>
